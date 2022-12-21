0cd1bfbdeb322be929b282ce9f8d6d1cde5a9235
Owein <owein.dourneau@gmail.com>
2022-12-21 12:11:22 +0100

    Fix ? error   + add files

--- scrapper.py
+++ scrapper.py
@@ -18,39 +18,54 @@ def read_credentials():
     return creds
 
 
+def get_rappers():
+    with open("rappers.txt", "r") as f:
+        rappers = []
+        for line in f:
+            rappers.append(line.strip())
+    return rappers
+
+
 def fetch_songs(artist_list, num_songs, main_folder):
     creds = read_credentials()
-    genius = lyricsgenius.Genius(creds["GENIUS_ACCESS_TOKEN"])
+    genius = lyricsgenius.Genius(creds["GENIUS_ACCESS_TOKEN"], timeout=40)
     genius.remove_section_headers = (
         True  # Remove section headers (e.g. [Chorus]) from lyrics when searching
     )
 
     for artist in artist_list:
         # Create artist's folder in main folder
-        artist_folder = os.path.join(main_folder, artist.replace(" ", "_"))
+
+        artist_folder = os.path.join(
+            main_folder, artist.translate(str.maketrans("", "", '!@#$"’'))
+        )
+
         if not os.path.exists(artist_folder):
             os.makedirs(artist_folder)
 
         # Search for artist's songs and save lyrics in individual files
-        try:
-            artist_songs = genius.search_artist(artist, num_songs)
-            try:
-                for song in artist_songs.songs:
-                    song_file = os.path.join(
-                        artist_folder, song.title.replace(" ", "_")
-                    )
-                    with open(song_file, "w", encoding="utf-8") as f:
-                        f.write(song.lyrics)
-            except:
-                print("Error when writting the song")
 
-        except:
-            print("Error: Artist '{}' not found".format(artist))
+        artist_songs = genius.search_artist(
+            artist,
+            num_songs,
+        )
+        for song in artist_songs.songs:
+            song_file = os.path.join(
+                artist_folder,
+                song.title.replace(" ", "_").translate(
+                    str.maketrans("", "", '!@#$"’?')
+                ),
+            )
+            song_file = str(song_file) + ".txt"
+            with open(song_file, "w", encoding="utf-8") as f:
+                f.write(song.lyrics)
 
 
 def main():
     # List of artists
-    artist_list = ["Ed Sheeran", "Adele"]
+    # artist_list = ["A Tribe Called Quest"]
+    # print(get_rappers())
+    artist_list = get_rappers()
 
     # Number of songs to fetch for each artist
     num_songs = 5
